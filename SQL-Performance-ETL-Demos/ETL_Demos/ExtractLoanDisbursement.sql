b4829db5c309faf467fb1985893a051c68fef9c4
deepseekfrombd <deepseekfrombd@gmail.com>
2025-07-16 21:43:47 +0600

    Update ExtractLoanDisbursement.sql

--- SQL-Performance-ETL-Demos/ETL_Demos/ExtractLoanDisbursement.sql
+++ SQL-Performance-ETL-Demos/ETL_Demos/ExtractLoanDisbursement.sql
@@ -1,5 +1,5 @@
 -- Extract raw loan disbursement data for Jan-Dec 2023
 SELECT *
 INTO Staging_LoanDisbursement
-FROM LoanDisbursement
-WHERE DisbursementDate BETWEEN '2023-01-01' AND '2023-12-31';
+FROM LoanAccount
+WHERE DisburseDate BETWEEN '2023-01-01' AND '2023-12-31';
